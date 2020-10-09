0c8da1c9266d1789752d209a6dbe02e52c96d8ff
saiteja-code <saitejakanaparthy@gmail.com>
2020-10-09 16:31:53 +0530

    last name

--- src/main/java/com/cg/userregistration/UserRegistration.java
+++ src/main/java/com/cg/userregistration/UserRegistration.java
@@ -9,14 +9,14 @@ public class UserRegistration {
     public static void main( String[] args )
     {
         System.out.println( "Hello" );
-        firstName();
+        lastName();
     }
-    public static void firstName() {
-        System.out.println("Enter First Name");
-        String fN = sc.nextLine();
-        if (Pattern.matches("^[A-Z]{1}[a-z]{2,}$", fN) == true)
-            System.out.println("Valid First Name");
+    public static void lastName() {
+        System.out.println("Enter Last Name");
+        String lN = sc.nextLine();
+        if (Pattern.matches("^[A-Z]{1}[a-z]{2,}$", lN) == true)
+            System.out.println("Valid Last Name");
         else
-            System.out.println("Invalid First Name");
+            System.out.println("Invalid Last Name");
     }
 }
