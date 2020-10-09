86ce137980793f312a89de5cf4c474bfb0f05aed
saiteja-code <saitejakanaparthy@gmail.com>
2020-10-09 16:52:28 +0530

    should clear all email validation cases

--- src/main/java/com/cg/userregistration/UserRegistration.java
+++ src/main/java/com/cg/userregistration/UserRegistration.java
@@ -8,19 +8,18 @@ public class UserRegistration {
     static Scanner sc=new Scanner(System.in);
 
 
-    public static void setPassword(String password) {
-        String regex = "^(?=^.).{8,}$";
-        if (password.matches(regex))
-            System.out.println("Password is Valid");
+    public static void email(String emailId) {
+        String regex = "^[_a-z0-9-\\+]+(\\.[_a-z0-9-]+)*@[a-z0-9]+(\\.[a-z0-9]+)*(\\.[a-z]{2,})$";
+        if (emailId.matches(regex))
+            System.out.println("EmailId is Valid");
         else
-            System.out.println("Password is Invalid");
+            System.out.println("EmailId is Invalid");
     }
-
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter the password");
-        String password = sc.nextLine();
-        setPassword(password);
+        System.out.println("Enter the EmailId");
+        String emailId = sc.nextLine();
+        email(emailId);
         sc.close();
     }
 
