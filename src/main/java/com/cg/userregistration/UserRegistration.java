148c11c84f821a4bdc650c740908f59ad708f85c
saiteja-code <saitejakanaparthy@gmail.com>
2020-10-09 17:02:50 +0530

    introducing junit test cases

--- src/main/java/com/cg/userregistration/UserRegistration.java
+++ src/main/java/com/cg/userregistration/UserRegistration.java
@@ -5,22 +5,58 @@ import java.util.regex.Pattern;
 
 public class UserRegistration {
 
-    static Scanner sc=new Scanner(System.in);
 
 
-    public static void email(String emailId) {
-        String regex = "^[_a-z0-9-\\+]+(\\.[_a-z0-9-]+)*@[a-z0-9]+(\\.[a-z0-9]+)*(\\.[a-z]{2,})$";
-        if (emailId.matches(regex))
-            System.out.println("EmailId is Valid");
+
+    // UC1 and UC2 - First name starts with Cap and has minimum 3 characters
+    public  boolean names(String name) {
+        return (Pattern.matches("^[A-Z]{1}[a-z]{2,}$", name) == true);
+    }
+
+
+
+    /*
+     * UC 3 - enter a valid email UC 9 - Cleared All Sample Provided
+     */
+    public boolean email(String emailId) {
+        return (Pattern.matches("^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9]+(?:\\.[a-zA-Z0-9]+)*$", emailId) == true);
+    }
+
+    // UC4 - Mobile Format
+    public boolean mobile(String mobileNum) {
+        return(Pattern.matches("^[1-9]{2}\\s{1}[1-9]{1}[0-9]{9}$", mobileNum) == true);
+    }
+
+    // UC 5 to 8 - Password
+    public boolean password(String pass) {
+        return (Pattern.matches("^(?=.*\\d)(?=.*[a-z])(?=.*[A-Z])(?!.*\\s)[a-zA-Z0-9]*[^a-zA-Z0-9\\s]{1}[a-zA-Z0-9]*$",
+                pass) == true);
+    }
+
+    public String checkMood(String word) {
+        if (word.contains("Sad"))
+            return "Sad";
         else
-            System.out.println("EmailId is Invalid");
+            return "Happy";
     }
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Enter the EmailId");
-        String emailId = sc.nextLine();
-        email(emailId);
-        sc.close();
+    Scanner scanner = new Scanner(System.in);
+    UserRegistration ur = new UserRegistration();
+        System.out.println("Welcome to User Registration Program");
+            System.out.println("Enter First name");
+            System.out.println("First Name: "+ ur.names(scanner.nextLine()));
+            System.out.println("Enter Last name");
+            System.out.println("Last Name: "+ ur.names(scanner.nextLine()));
+            System.out.println("Enter EmailId");
+            System.out.println("EmailId: "+ ur.email(scanner.nextLine()));
+            System.out.println("Enter Mobile Number");
+            System.out.println("Mobile Number: "+ ur.mobile(scanner.nextLine()));
+            System.out.println("Enter Password");
+            System.out.println("Password: "+ ur.password(scanner.nextLine()));
+            System.out.println("How is your Mood today");
+            System.out.println(ur.checkMood(scanner.nextLine()));
+
+
     }
 
 }
--- src/test/java/com/cg/userregistration/UserRegistrationTest.java
+++ src/test/java/com/cg/userregistration/UserRegistrationTest.java
@@ -4,6 +4,33 @@ import org.junit.Assert;
 import org.junit.Test;
 
 public class UserRegistrationTest {
+    UserRegistration ur=new UserRegistration();
 
+    @Test
+    public void firstNameValidation(){
+        Assert.assertTrue(ur.names("Sai"));
+    }
+    @Test
+    public void lastNameValidation(){
+        Assert.assertTrue(ur.names("Teja"));
+    }
+
+    @Test
+    public void emailValidation(){
+        Assert.assertTrue(ur.email("Sai.Teja@nitt.edu"));
+    }
+    @Test
+    public void mobileValidation(){
+        Assert.assertTrue(ur.mobile("91 9494993238"));
+    }
+    @Test
+    public void passwordValidation(){
+        Assert.assertTrue(ur.password("Saitej@4528"));
+    }
+
+    @Test
+    public void should_returnSad(){
+        Assert.assertEquals("Sad",ur.checkMood("Sad"));
+    }
 
 }
